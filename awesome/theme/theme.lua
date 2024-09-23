cea21f3199c5cfa398aa02d3d9d21b6f6d514e39
Nikita Rudakov <boltgopnik@gmail.com>
2024-09-23 23:11:02 +0300

    better colors

--- awesome/theme/theme.lua
+++ awesome/theme/theme.lua
@@ -24,10 +24,10 @@ theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
-theme.useless_gap   = dpi(0)
-theme.border_width  = dpi(1)
+theme.useless_gap   = dpi(3)
+theme.border_width  = dpi(2)
 theme.border_normal = "#000000"
-theme.border_focus  = "#ffdf04"
+theme.border_focus  = "#e09eda"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
