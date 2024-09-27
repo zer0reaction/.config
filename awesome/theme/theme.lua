cc2b2cfde13d1a369fbbf1ea1f151098b67fc9aa
Nikita Rudakov <boltgopnik@gmail.com>
2024-09-27 17:45:55 +0300

    new background

--- awesome/theme/theme.lua
+++ awesome/theme/theme.lua
@@ -27,7 +27,7 @@ theme.fg_minimize   = "#ffffff"
 theme.useless_gap   = dpi(3)
 theme.border_width  = dpi(2)
 theme.border_normal = "#000000"
-theme.border_focus  = "#e09eda"
+theme.border_focus  = "#E0E3EA"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
@@ -97,7 +97,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/home/zer0/.config/awesome/theme/background.png"
+theme.wallpaper = "/home/zer0/.config/awesome/theme/background2.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
