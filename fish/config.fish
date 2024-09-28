bc367199ffe3eddeaae60ede1964edc6a334d74c
Nikita Rudakov <boltgopnik@gmail.com>
2024-09-28 10:05:32 +0300

    vim nvim alias

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ alias rm "echo use trash or /bin/rm"
 alias tp trash-put
 alias tl trash-list
 alias te trash-empty
+alias vim nvim
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
