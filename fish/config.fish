e72b459038a7abf7326663bfc93c8112ba3e60d7
Nikita Rudakov <boltgopnik@gmail.com>
2024-10-08 19:32:46 +0300

    changed trashcli alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,10 @@
 set fish_greeting ""
 
 alias rm "echo use trash or /bin/rm"
-alias tp trash-put
-alias tl trash-list
-alias te trash-empty
+alias t-p trash-put
+alias t-l trash-list
+alias t-r trash-restore
+alias t-e trash-empty
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
