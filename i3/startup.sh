c7a153388c9494f73187ca5aad91fc9d68ad0fc2
Nikita Rudakov <boltgopnik@gmail.com>
2024-10-08 18:39:36 +0300

    changed startup script

--- i3/startup.sh
+++ i3/startup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+mouse_id=9
+
+# Startup programs
 setxkbmap -option grp:alt_shift_toggle -option caps:escape -layout us,ru
-/usr/bin/lxpolkit
-nm-applet
-xfce4-clipman
+/usr/bin/lxpolkit &
+nm-applet &
+xfce4-clipman &
+
+# Mouse configuration
+xinput set-prop $mouse_id "Coordinate Transformation Matrix" 1, 0, 0, 0, 1, 0, 0, 0, 1
+xinput set-prop $mouse_id "libinput Accel Profile Enabled" 0 1 0
+
+# Computer specific configurations
+xrandr --output HDMI-1 --off
+xrandr --output DP-3 --mode 1920x1080 --rate 240
