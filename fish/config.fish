e7de4a84c522ede70dbbcb3cce2dadef1904b45e
Nikita Rudakov <boltgopnik@gmail.com>
2025-01-11 22:44:38 +0300

    added aliases for fast directory travel

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,8 @@ alias t-p trash-put
 alias t-l trash-list
 alias t-r trash-restore
 alias t-e trash-empty
+alias prog "cd ~/Documents/programming"
+alias sand "cd ~/Documents/programming/sandbox"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
