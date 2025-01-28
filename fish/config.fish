b69c16168072eda0f8900f5596880c095d1ccf96
zer0reaction <boltgopnik@gmail.com>
2025-01-28 08:58:11 +0300

    updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,13 @@
 set fish_greeting ""
 
 alias rm "echo use trash or /bin/rm"
+
 alias t-p trash-put
 alias t-l trash-list
 alias t-r trash-restore
 alias t-e trash-empty
-alias prog "cd ~/Programming"
-alias sand "cd ~/Programming/sandbox"
-alias stud "cd ~/Documents/study"
+
+alias nv nvim
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
